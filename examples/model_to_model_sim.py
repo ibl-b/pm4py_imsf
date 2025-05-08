72260519979d3b5067b6ba47aa5996ad947d4e2f
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-05-08 11:26:57 +0200

    updated example

--- examples/model_to_model_sim.py
+++ examples/model_to_model_sim.py
@@ -15,20 +15,25 @@ def execute_script():
     # computes the footprints-based behavioral similarity between a process tree
     # and a POWL model
     behavioral_similarity = pm4py.behavioral_similarity(powl, process_tree)
-    print(behavioral_similarity)
+    print("behavioral similarity", behavioral_similarity)
 
     # computes the structural similarity between a POWL and a process tree
     structural_similarity = pm4py.structural_similarity(process_tree, powl)
-    print(structural_similarity)
+    print("structural similarity", structural_similarity)
+
+    # computes the label-sets-similarity between a process tree and a POWL
+    label_sets_similarity = pm4py.label_sets_similarity(powl, process_tree)
+    print("label sets similarity", label_sets_similarity)
 
     try:
         # computes the embeddings-based similarity between a process tree and a POWL
         embeddings_similarity = pm4py.embeddings_similarity(powl, process_tree)
-        print(embeddings_similarity)
+        print("embeddings similarity", embeddings_similarity)
+
         # embeddings are non-deterministic, so even comparing a model with itself,
         # it would probably give a value below 1
         embeddings_similarity2 = pm4py.embeddings_similarity(process_tree, process_tree)
-        print(embeddings_similarity2)
+        print("embeddings similarity", embeddings_similarity2)
     except:
         traceback.print_exc()
 
